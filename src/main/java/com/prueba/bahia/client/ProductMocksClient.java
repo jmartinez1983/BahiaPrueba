a47df0f535168ae7f1c810d6cbafac142d8c97f3
josele3003@hotmail.com <josele3003@hotmail.com>
2022-08-21 19:50:22 +0200

    Init commit

--- src/main/java/com/prueba/bahia/client/ProductMocksClient.java
+++ src/main/java/com/prueba/bahia/client/ProductMocksClient.java
@@ -1,6 +1,5 @@
 package com.prueba.bahia.client;
 
-import com.prueba.bahia.domain.ProductDetails;
 import com.prueba.bahia.model.InlineResponse200;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
